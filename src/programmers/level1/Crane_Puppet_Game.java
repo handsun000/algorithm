bc4c8f89955cc5ff4c14b1306bee124c02fc4acf
handsun000 <handsun000@github>
2021-12-02 16:49:46 +0900

    Crane_Puppet_Game

--- src/programmers/level1/Crane_Puppet_Game.java
+++ src/programmers/level1/Crane_Puppet_Game.java
@@ -1,32 +1,43 @@
 package programmers.level1;
+
 import java.util.Stack;
 
 class Crane_Puppet_Game {
-        public int solution(int[][] board, int[] moves) {
-            int answer = 0;
-            Stack<Integer> st = new Stack<>();
+    public static void main(String[] args) {
+        int[][] board = {{1, 2, 3}, {1, 2, 3}};
+        int[] moves = {1, 2, 3};
 
-            st.push(-1);
+        int solution = solution(board, moves);
 
-            for(int i = 0; i<moves.length; i++) {
-                for(int j = 0; j<board[0].length; j++) {
-                    if(board[j][moves[i]-1] == 0) continue;
-                    else {
-                        int pk = st.peek();
-                        st.push(board[j][moves[i]-1]);
+        System.out.println("solution = " + solution);
+    }
 
-                        if (board[j][moves[i]-1] == pk) {
-                            st.pop();
-                            st.pop();
-                            answer += 2;
-                        }
-                        board[j][moves[i]-1] = 0;
-                        break;
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> st = new Stack<>();
+
+        st.push(-1);
+
+        for (int i = 0; i < moves.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[j][moves[i] - 1] == 0) continue;
+                else {
+                    int pk = st.peek();
+                    st.push(board[j][moves[i] - 1]);
+
+                    if (board[j][moves[i] - 1] == pk) {
+                        st.pop();
+                        st.pop();
+                        answer += 2;
                     }
+                    board[j][moves[i] - 1] = 0;
+                    break;
                 }
             }
+        }
 
-            return answer;
+
+        return answer;
 
     }
 }
