a80a48cd19bebc5df299ad82a65c8f80d554adb4
son <handsun000@gmail.com>
2024-11-29 14:23:06 +0900

    몫 구하기

---
+++ src/programmers/level0/몫구하기.java
@@ -0,0 +1,12 @@
+package programmers.level0;
+
+public class 몫구하기 {
+    public static void main(String[] args) {
+        System.out.println(solution(10, 5));
+
+    }
+    static public int solution(int num1, int num2) {
+
+        return num1/num2;
+    }
+}
