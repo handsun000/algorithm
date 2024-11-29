e406301910e40797599deadc5b34bf4b14c50825
son <handsun000@gmail.com>
2024-11-29 14:21:59 +0900

    나머지 구하기

---
+++ src/programmers/level0/나머지구하기.java
@@ -0,0 +1,10 @@
+package programmers.level0;
+
+public class 나머지구하기 {
+    public static void main(String[] args) {
+        System.out.println(solution(3, 4));
+    }
+    static public int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+}
