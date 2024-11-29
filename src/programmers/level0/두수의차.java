523ce0712a622720803a834f214ae3ad2fe8b538
son <handsun000@gmail.com>
2024-11-29 14:22:41 +0900

    두 수의 차

---
+++ src/programmers/level0/두수의차.java
@@ -0,0 +1,15 @@
+package programmers.level0;
+
+public class 두수의차 {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(solution(num1, num2));
+    }
+
+    static public int solution(int num1, int num2) {
+
+        return num1 - num2;
+    }
+}
