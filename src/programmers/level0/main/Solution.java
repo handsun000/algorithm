d290c1960e82448e1bc5cd5cd30b3a9c2ea41325
son <handsun000@gmail.com>
2025-04-17 19:00:50 +0900

    work

--- src/programmers/level0/main/Solution.java
+++ src/programmers/level0/main/Solution.java
@@ -1,37 +1,66 @@
-package programmers.level0.main;
-
+package programmers.level0.main;// 3210123
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] jobs) {
-        Arrays.sort(jobs, Comparator.comparingInt(o -> o[0]));
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        int[] point = {3,2,1,0,1,2,3};
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1[1] != o2[1]) return Integer.compare(o1[1], o2[1]);
-            if (o1[0] != o2[0]) return Integer.compare(o1[0], o2[0]);
-            return 0;
-        });
+        Map<String, Integer> rt = new HashMap<>();
+        rt.put("r", 0);
+        rt.put("t", 0);
+        Map<String, Integer> cf = new HashMap<>();
+        cf.put("c", 0);
+        cf.put("f", 0);
+        Map<String, Integer> jm = new HashMap<>();
+        jm.put("j", 0);
+        jm.put("m", 0);
+        Map<String, Integer> an = new HashMap<>();
+        an.put("a", 0);
+        an.put("n", 0);
 
-        int time = 0;
-        int idx = 0;
-        int total = 0;
-        int count = 0;
+        for (int i = 0; i<survey.length; i++) {
+            String s = survey[i];
+            String s0 = String.valueOf(s.charAt(0));
+            String s1 = String.valueOf(s.charAt(1));
 
-        while (count < jobs.length) {
-            while (idx < jobs.length && jobs[idx][0] <= time) {
-                pq.offer(jobs[idx++]);
+            int c = choices[i]-1;
+
+            if (c < 4) {
+                if (s.contains("r")) {
+                    rt.put(s0, rt.get(s0)+point[c]);
+                }
+                else if (s.contains("c")) {
+                    cf.put(s0, rt.get(s0)+point[c]);
+                }
+                else if (s.contains("j")) {
+                    jm.put(s0, rt.get(s0)+point[c]);
+                }
+                else if (s.contains("a")) {
+                    an.put(s0, rt.get(s0)+point[c]);
+                }
             }
-
-            if (!pq.isEmpty()) {
-                int[] job = pq.poll();
-                time += job[1];
-                total += time - job[0];
-                count++;
-            } else {
-                time = jobs[idx][0];
+            else if (4 < c) {
+                if (s.contains("r")) {
+                    rt.put(s1, rt.get(s1)+point[c]);
+                }
+                else if (s.contains("c")) {
+                    cf.put(s1, rt.get(s1)+point[c]);
+                }
+                else if (s.contains("j")) {
+                    jm.put(s1, rt.get(s1)+point[c]);
+                }
+                else if (s.contains("a")) {
+                    an.put(s1, rt.get(s1)+point[c]);
+                }
             }
         }
 
-        return total / jobs.length;
+        System.out.println(rt);
+        System.out.println(cf);
+        System.out.println(jm);
+        System.out.println(an);
+
+        return answer;
     }
 }
