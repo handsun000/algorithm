8da5c86582100b1ff9841f174c38a74961b2da08
son <handsun000@gmail.com>
2025-05-11 23:46:39 +0900

    나머지구하기

--- src/programmers/level0/나머지구하기.java
+++ src/programmers/level0/나머지구하기.java
@@ -2,7 +2,7 @@ package programmers.level0;
 
 public class 나머지구하기 {
     public static void main(String[] args) {
-        System.out.println(solution(3, 4));
+        System.out.println(solution(5, 7));
     }
     static public int solution(int num1, int num2) {
         return num1 % num2;
